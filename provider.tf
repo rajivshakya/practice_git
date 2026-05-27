df672e58111ffdeae06bf2ce86a9a04a4da7bd9d
Rajiv Shakya <rajivshaky@gmail.com>
2026-05-27 09:49:50 +0530

    "update backend"

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "tcs-rg"
+    location = "Central India"
+    storage_account_name = "rajivshakyasa687"
+    container_name = "terrafom.tfstate"
+  }
+}
 provider "azurerm" {
   features {
     
